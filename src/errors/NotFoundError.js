99c9b5aa85bd2137fc6c71f7e60c5b6085dd0710
forest-kimino <forest.kimino@ymail.ne.jp>
2025-05-29 23:03:15 +0900

    ファイルコメントを追加

--- src/errors/NotFoundError.js
+++ src/errors/NotFoundError.js
@@ -1,6 +1,15 @@
 /**
- * NotFoundError
- * 指定情報が見当たらない場合の業務上のエラー
+ * @file カスタムエラークラス：NotFoundError
+ *
+ * このクラスは、リソースが見つからなかった場合に使用する HTTP 404 用のエラークラスです。
+ * 標準の Error クラスを拡張し、エラーハンドリング時に一貫した形式で利用されます。
+ *
+ * 使用例：
+ *   throw new NotFoundError('指定されたカフェが見つかりませんでした');
+ *
+ * プロパティ：
+ *   - name: エラー名（'NotFoundError'）
+ *   - statusCode: HTTP ステータスコード（404）
  */
 class NotFoundError extends Error {
   constructor(message) {
